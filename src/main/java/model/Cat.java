f4ab39ea00652d1895bd0fe0b491cf0e5f8f5172
jibes <jibes@Jibesh_PC>
2025-05-01 13:36:15 -0500

    added cat dog rabbit and exotic animal classes

---
+++ src/main/java/model/Cat.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Cat extends Pet{
+	public Cat(int id, String name, String type, String species, int age) {
+		super(id,name,"Cat", species,age);
+	}
+}
---
+++ src/main/java/model/Dog.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Dog extends Pet{
+
+	public Dog(int id, String name, String type, String species, int age) {
+		super(id,name,"Dog", species,age);
+	}
+	
+}
