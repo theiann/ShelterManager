5fc9d0ced029e068b656633473675ea9e739e8c2
jibes <jibes@Jibesh_PC>
2025-05-02 14:00:53 -0500

    added methods in main to test JSON loaders

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,44 @@
 package main;
 
-import view.UserView;
+import java.io.IOException;
+import java.util.List;
+
+import model.ExoticAnimal;
+import model.Manager;
+import model.Pet;
+import model.Shelter;
+
 public class Main {
 
-	public static void main(String[] args) {
-		UserView view = new UserView();
+	public static void main(String[] args) throws IOException {
+		Shelter <Pet> newShelter = new Shelter<>(); 
+		Manager dataManager = new Manager(); 
+		
+		//Loads and prints pets loaded from pets.JSON
+		List<Pet> loadedPets =dataManager.loadPets();
+		System.out.println ("Loaded "+loadedPets.size() + "pets from Pets.JSON:");
+		//printPets(loadedPets);
+	    
+		//Loads and prints pets loaded from exoticanimal.JSON
+		List<ExoticAnimal>exotics = dataManager.loadExoticAnimals();
+		List<Pet> adaptedExotics = dataManager.adaptExoticAnimals(exotics);
+		System.out.println ("");
+        System.out.println ("Loaded "+ adaptedExotics.size() + "exotic animals from exotic_animals.JSON:");
+       // printPets(adaptedExotics);
+        
+        
+        
 	}
+	
+	
+	private static void printPets(List<Pet> pets) {
+        for (Pet pet : pets) {
+            System.out.println(pet.getID() + ": " + pet + (pet.isAdopted() ? " (Adopted)" : " (Available)"));
+        }
+    }
+	
 
-}
+	}
+		
+
+
