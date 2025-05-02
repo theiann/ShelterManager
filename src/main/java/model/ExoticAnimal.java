2a6bd2d66938c7267119a6f404cbf4a9c9613570
jibes <jibes@Jibesh_PC>
2025-05-01 21:42:12 -0500

    added constructor and getters and setters for exoticAnimal.java

--- src/main/java/model/ExoticAnimal.java
+++ src/main/java/model/ExoticAnimal.java
@@ -1,5 +1,57 @@
 package model;
 
-public class ExoticAnimal extends Pet{
+public class ExoticAnimal {
+	private String uniqueId;
+    private String animalName;
+    private String category;
+    private String subSpecies;
+    private int yearsOld;
 
+    public ExoticAnimal(String uniqueId, String animalName, String category, String subSpecies, int yearsOld) {
+        this.uniqueId = uniqueId;
+        this.animalName =animalName;
+        this.category =category;
+        this.subSpecies =subSpecies;
+        this.yearsOld = yearsOld;
+    }
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId =uniqueId;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName =animalName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category =category;
+    }
+
+    public String getSubSpecies() {
+        return subSpecies;
+    }
+
+    public void setSubSpecies(String subSpecies) {
+        this.subSpecies = subSpecies;
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
 }
